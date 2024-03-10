7f48fcb48580012264db8dc91b348595d1cecfec
jnopareboateng <jnopareboateng@outlook.com>
2024-03-10 08:12:53 +0000

    Fix formatting in model.py

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ my_credentials = {
     "url"    : "https://us-south.ml.cloud.ibm.com",
     "token" : "skills-network" # <--- NOTE: specify "skills-network" as your token (NOT as your apikey) 
 }
-
+    
 params = {
         GenParams.MAX_NEW_TOKENS: 256, # The maximum number of tokens that the model can generate in a single run.
         GenParams.TEMPERATURE: 0.1,   # A parameter that controls the randomness of the token generation. A lower value makes the generation more deterministic, while a higher value introduces more randomness.
